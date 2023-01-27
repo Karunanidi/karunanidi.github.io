937791cdb451c6c4ebf025707bbb7589e44c3b39
Karunanidi <trisna.na@gmail.com>
2023-01-27 20:18:48 +0700

    Update Js

--- hbd/js/main.js
+++ hbd/js/main.js
@@ -2,7 +2,7 @@
 
       var background = "https://l.top4top.io/p_2583x9f5k1.png";
       var foto = "https://l.top4top.io/p_2583x9f5k1.png";
-      var musik = "https://g.top4top.io/m_2583vznpu1.mp3";
+      var musik = "music.mp3";
       var panggilan = "Cinta";
       var ucapan = "selamat ulang tahun, sehat selalu. gw suka sama lu tod!";
 
